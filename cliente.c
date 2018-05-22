67bb63a88686467e866bcaa4e9edf4f30bd92155
maxaraujo <maximillian.alex@gmail.com>
2018-05-22 16:21:53 -0300

    Fomulação da entrutura base do cliente

--- cliente.c
+++ cliente.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <string.h>
+#include <netdb.h>
+
+/*
+** REDES DE COMPUTADORE 2018/1 - Trabalho prático 2 
+** Maximilliam Alex de Araujo
+** Pratica com biblioteca socket utilizando protocolo UDP (IPv4 e  IPv6)
+*/
+
+/*
+** Constantes
+*/
+#define LARGURA_BUFFER 100
+
+/*
+** Recebe como parâmetro <ip/nome> <porta>
+** Exemplo: ./cliente ::1 9999
+*/
+void main(int argc, char *argv[])
+{
+	/*
+	** Variáveis e estruturas de dados
+	*/
+	int sd, rc;
+	char servidor[NETDB_MAX_HOST_NAME_LENGTH];
+	char buffer[LARGURA_BUFFER];
+	struct sockaddr_in6 servidoraddr;
+	int lservidoraddr = sizeof(servidoraddr);
+	struct addrinfo hints, *res;
+
+	/*
+	** Loop para facilitar a limpeza de erros
+	*/
+	do
+	{
+		
+	};
+}
+
