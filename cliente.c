8ad24542e23f5f60af738fa78e8c63bd3a883865
maxaraujo <maximillian.alex@gmail.com>
2018-05-23 19:02:13 -0300

    validacões do servidor no cliente

--- cliente.c
+++ cliente.c
@@ -16,6 +16,8 @@
 ** Constantes
 */
 #define LARGURA_BUFFER 100
+#define NUM_DE_PARAMETROS 3
+#define ENDERECO_VALIDO 1
 
 /*
 ** Recebe como parâmetro <ip/nome> <porta>
@@ -26,18 +28,59 @@ void main(int argc, char *argv[])
 	/*
 	** Variáveis e estruturas de dados
 	*/
-	int sd, rc;
+	int sd, validador, porta;
 	char servidor[NETDB_MAX_HOST_NAME_LENGTH];
 	char buffer[LARGURA_BUFFER];
-	struct sockaddr_in6 servidoraddr;
-	int lservidoraddr = sizeof(servidoraddr);
-	struct addrinfo hints, *res;
+	struct in6_addr servidoraddr;
+	struct addrinfo dicas, *res = NULL;
 
 	/*
 	** Loop para facilitar a limpeza de erros
 	*/
 	do
 	{
+		if(argc != NUM_DE_PARAMETROS)
+		{
+			printf("Digite todos os parâmetro. Ex.: ./cliente <ip/nome> <porta>!\n");
+			return 0;
+		};
+
+		strcpy(servidor, argv[1]);
+		porta = atoi(argv[2]);
+
+		memset(&dicas, 0x00, sizeof(dicas));
+		dicas.ai_flags = AI_NUMERICSERV;
+		dicas.ai_family = AF_UNSPEC;
+		dicas.ai_socktype = SOCK_DGRAM;
+
+		/*
+		** Preenchendo a estrutura do cabeçalho IPv4 ou IPv6
+		*/
+		validador = inet_pton(AF_INET, servidor, &servidoraddr);
+		if(validador == ENDERECO_VALIDO)
+		{
+			dicas.ai_family = AF_INET;
+			dicas.ai_flags |= AI_NUMERICHOST;
+		}else{
+			validador = inet_pton(AF_INET6, servidor, &servidoraddr);
+			if(validador == ENDERECO_VALIDO){
+				dicas.ai_family = AF_INET6;
+				dicas.ai_flags |= AI_NUMERICHOST;
+			};
+		};
+		/*
+		** Se algo deu errado no preechimento, é suposto que o endereço é inválido
+		*/
+		validador = getaddrinfo(servidor, porta, &dicas, &res);
+		if(validador != 0)
+		{
+			printf("Problema ao identificar o servidor. Erro: %s\n", gai_strerror(validador));
+			if(validador == EAI_SYSTEM){
+				perror("getaddrinfo() failed");			
+			};
+			return 0;
+		};
+
 		
 	};
 }
